210f801a0c715700f7530d10c60599573b4aca49
Roger Ledgister <roger.ledgister@gmail.com>
2020-06-30 10:29:02 -0700

    Added Complete function for assignment, cleaned up comments

--- Assignment1.R
+++ Assignment1.R
@@ -3,10 +3,10 @@ library(stringr)
 pollutantmean <- function(directory,pollutant, id=1:332) {
   dat = data.frame() #Create empty dataframe
   for(currentid in id){
+    #Push data from each monitor into dat to create a complete monitor file
     filename <- paste(directory,str_pad(currentid,3,pad="0"),".csv",sep="")
     print(currentid)
     dat <- rbind(dat, read.csv(filename))
-    #Push data from each monitor into dat to create a complete monitor file
   }
   poldat <- dat[[pollutant]]
   poldat <- poldat[!is.na(poldat)]
@@ -14,3 +14,13 @@ pollutantmean <- function(directory,pollutant, id=1:332) {
   mean(poldat)
 }
 
+complete <- function(directory,id=1:332) {
+  nobs = numeric()
+  for(currentid in id){
+    filename <- paste(directory,str_pad(currentid,3,pad="0"),".csv",sep="")
+    print(currentid)
+    moni <- read.csv(filename)
+    nobs <- c(nobs,nrow(moni[complete.cases(moni),]))
+  }
+  data.frame(id=id,nobs=nobs)
+}
